99372900e0afce90175d91d79dad3afdc725e21b
Robin Pillar <rpillar72@gmail.com>
2019-12-13 17:28:35 +0000

    created button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+display: inline-block;
+padding: 0.5rem 0;
+margin: 0.5rem 1rem;
+width: 11rem;
+background: transparent;
+color: black;
+border: 2px solid black;
+font-family: 'Montserrat', sans-serif;
+`
+export default Button;
