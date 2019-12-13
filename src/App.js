fbf077d760772e93637ab8a103b69599b21098a6
Robin Pillar <rpillar72@gmail.com>
2019-12-13 14:47:29 +0000

    rendered ArticleList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import ArticleList from './components/ArticleList';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Header />
         <Navbar />
+        <ArticleList />
       </div>
     );
   }
