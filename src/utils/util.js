b873f8aa21aa54739038d75412adb3d74bf139f5
Robin Pillar <rpillar72@gmail.com>
2019-12-13 14:47:48 +0000

    created getAllArticles util function

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,9 +1,15 @@
 const axios = require('axios');
 
+const baseURL = 'https://nc-news-rpillar.herokuapp.com/api';
+
 exports.getAllTopics = () => {
-  return axios
-    .get('https://nc-news-rpillar.herokuapp.com/api/topics')
-    .then(({ data }) => {
-      return data.topics;
-    });
+  return axios.get(`${baseURL}/topics`).then(({ data }) => {
+    return data.topics;
+  });
+};
+
+exports.getAllArticles = () => {
+  return axios.get(`${baseURL}/articles`).then(({ data }) => {
+    return data.articles;
+  });
 };
