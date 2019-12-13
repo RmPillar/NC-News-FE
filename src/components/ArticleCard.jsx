ad8d6d30ec14f18f33d4a91a9b74863624db01ef
Robin Pillar <rpillar72@gmail.com>
2019-12-13 17:45:50 +0000

    added styled article component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {Link} from '@reach/router'
+import styled from 'styled-components'
 
 const ArticleCard = ({articleData:{article_id, title,author,topic,created_at,votes,comment_count}}) => {
+    
+    const Article = styled.article`
+        padding: 0.5rem 0;
+        margin: 0.5rem 1rem;
+        width: 50vw;
+        background: white;
+        color: black;
+        border: 2px solid black;
+        font-family: 'Montserrat', sans-serif;
+    `
     return (
-        <article>
+        <Article>
             <Link id='name' to={`articles/${article_id}`}>
                 <h4>{title}</h4>
             </Link>
@@ -12,7 +23,7 @@ const ArticleCard = ({articleData:{article_id, title,author,topic,created_at,vot
             <p>Posted At: {created_at}</p>
             <p>Votes: {votes}</p>
             <p>Comments: {comment_count}</p>
-        </article>
+        </Article>
     );
 };
 
