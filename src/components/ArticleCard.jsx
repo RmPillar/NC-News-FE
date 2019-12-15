7bb880e05e88d8636b79ae6eab04041556103311
Robin Pillar <rpillar72@gmail.com>
2019-12-15 15:55:44 +0000

    styled article cards

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,41 +2,65 @@ import React from 'react';
 import {Link} from '@reach/router'
 import styled from 'styled-components'
 
+
 const ArticleCard = ({articleData:{article_id, title,author,topic,created_at,votes,comment_count},color}) => {
     
     const Article = styled.article`
-
-        background: ${`#${color}`};
-        
-
-
         padding: 0.5rem 0;
         margin: 0.5rem 1rem;
         border-radius: 10px;
-        color: white;
-        width: 35vw;
-        border: 2px white;
+        color: whitesmoke;
+        background: ${`#${color}`};
+        width: 70vw;
+        height: 200px;
+        border: 2px whitesmoke;
+        font-family: 'Montserrat', sans-serif;
+        display:flex;
+        align-items: center;
+        justify-content: space-between;
+    `
+
+    const Button = styled.button`
+        display: inline-block;
+        padding: 0.5rem 0;
+        margin: 0.5rem 1rem;
+        width: 5rem;
+        background: whitesmoke;
+        color: black;
+        border: 2px solid whitesmoke;
+        border-radius: 10px;
         font-family: 'Montserrat', sans-serif;
     `
 
-    const H4 = styled.h4`
-    color:black;
+    const H5 = styled.h5`
+    color:white;
         text-decoration: linethrough
         &:hover {
             text-decoration: underline; 
         };
     `
 
+
+
     return (
         <Article>
-            <Link id='name' to={`articles/${article_id}`}>
-                <H4>{title}</H4>
-            </Link>
-            <h6>{author}</h6>
-            <p>{topic}</p>
-            <p>Posted At: {created_at}</p>
-            <p>Votes: {votes}</p>
-            <p>Comments: {comment_count}</p>
+            <section id='title'>
+                <Link id='name' to={`articles/${article_id}`}>
+                    <H5>{title}</H5>
+                </Link>
+                <h6>{author}</h6>
+                <p>{topic}</p>
+            </section>
+            <section id='info'>
+                <p>Posted At: {created_at}</p>
+                <p>Votes: {votes}</p>
+                <p>Comments: {comment_count}</p>
+            </section>
+            <section id='buttons'>
+                <Button>+</Button>
+                <Button>-</Button>
+                <Button>Comment</Button>
+            </section>
         </Article>
     );
 };
