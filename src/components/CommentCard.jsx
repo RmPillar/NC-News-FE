2b680a643c483f9cb5a3c04f6ad63897e257e8e1
Robin Pillar <rpillar72@gmail.com>
2019-12-16 07:37:58 +0000

    styled comment card to match rest of the site

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import styled from 'styled-components'
 
 const CommentCard = ({comment}) => {
+
+    const Section = styled.section`
+        padding: 0.5rem 0;
+        margin: 0.5rem 1rem;
+        border-radius: 10px;
+        color: #3e3e3e;
+        background: transparent;
+        width: 35vw;
+        min-width: 300px;
+        height: 200px;
+        border: 2px solid #26547C;
+        font-family: 'Roboto', sans-serif;
+    `
+
+
+
     return (
-        <section>
+        <Section>
             <p>{comment.author}</p>
             <p>{comment.body}</p>
             <p>{comment.created_at}</p>
             <p>{comment.votes}</p>
-        </section>
+        </Section>
     );
 };
 
