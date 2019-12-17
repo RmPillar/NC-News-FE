c70481f6bbb2c0ab760073643aa8e3bade1d24b4
Robin Pillar <rpillar72@gmail.com>
2019-12-17 12:59:38 +0000

    removed unneeded Router

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class App extends Component {
         <Navbar />
         <Router>
           <ArticleList path='/articles/*' user={this.state.user} />
-          <ArticleList path='/articles?:sort_by' user={this.state.user} />
           <ArticleList path='topic/:topicSlug/' />
           <ArticleList path='user/:username/' />
         </Router>
