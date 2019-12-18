2c5d1d095fb4db8bc0944d60bf9ca72a23dd8be2
Robin Pillar <rpillar72@gmail.com>
2019-12-18 15:36:31 +0000

    added styled buttons to NavBAr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Link} from '@reach/router'
 import * as api from '../utils/api'
 import {Nav} from '../styledComponents/Nav'
+import { TextField, MenuItem, Button} from '@material-ui/core';
 
 class Navbar extends Component {
 
@@ -23,33 +24,37 @@ class Navbar extends Component {
         this.setState({topicFilter:event.target.value})
     }
 
+    style = {
+        color: `#26547C`,
+        border: `2px solid #26547C`, 
+        margin: '20px'
+    }
+
+
     render() {
         const {topics, topicFilter} = this.state
         return (
             <div>
                 <Nav>
-                <Link to='/articles/new-article'><button>New Article</button></Link>
+                <Link to='/articles/new-article'><Button variant='outlined' style={this.style}>Create Article</Button></Link>
                     <form onSubmit={this.props.handleSubmit}>
-                        <label>
-                            Sort By: 
-                            <select>
-                                <option value='created_at'>Date Created</option>
-                                <option value='comment_count'>Number of Comments</option>
-                                <option value='votes'>Votes</option>
-                            </select>
-                            <button>Sort</button>
-                        </label>
+                        
+                            <TextField select helperText="Sort Articles By">
+                                <MenuItem value='created_at'>Date Created</MenuItem>
+                                <MenuItem value='comment_count'>Comment Count</MenuItem>
+                                <MenuItem value='votes'>Votes</MenuItem>
+                            </TextField>
+                            <Button variant='outlined' style={this.style}>Sort</Button>
+                      
                     </form>
                     <form>
-                        <label>
-                            Filter:
-                            <select onChange={this.handleChange}>
+                        
+                            <TextField select helperText="Filter Articles By Topic"onChange={this.handleChange}>
                                 {topics.map(({slug}) => {
-                                    return<option key={slug}>{slug}</option>
+                                    return<MenuItem key={slug} value={slug}>{slug}</MenuItem>
                                 })}
-                            </select>
-                            <Link to={`topic/${topicFilter}`}><button>Filter!</button></Link>
-                        </label>
+                            </TextField>
+                            <Link to={`topic/${topicFilter}`}><Button variant='outlined' style={this.style}>Filter!</Button></Link>
                     </form>
                 </Nav>
             </div>
--- src/styledComponents/Nav.jsx
+++ src/styledComponents/Nav.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
 export const Nav = styled.nav`
-    display: inline-block;
+    display: flex;
+    justify-content:space-between;
+    align-items:flex-end;
     padding: 0.5rem 0;
     margin: 0.5rem 1rem;
-    width: 90vw;
+    width: 95vw;
     background: transparent;
-    color: black;
-    border: 2px solid black;
     font-family: 'Roboto', sans-serif;
 `
