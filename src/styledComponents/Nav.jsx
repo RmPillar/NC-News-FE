8207e43ac5c65ee3649adf31426a511c8b577a6e
Robin Pillar <rpillar72@gmail.com>
2019-12-18 15:37:07 +0000

    added styled buttons to NavBAr

--- src/styledComponents/Nav.jsx
+++ src/styledComponents/Nav.jsx
@@ -2,8 +2,9 @@ import styled from 'styled-components'
 
 export const Nav = styled.nav`
     display: flex;
+    flex-wrap:wrap;
     justify-content:space-between;
-    align-items:flex-end;
+    align-items:center;
     padding: 0.5rem 0;
     margin: 0.5rem 1rem;
     width: 95vw;
