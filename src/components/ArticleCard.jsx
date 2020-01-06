c7aa345f0ced5398df99bb4eafef05946f61b3fe
Robin Pillar <rpillar72@gmail.com>
2020-01-06 12:44:46 +0000

    moved article styled component outside of articleCard component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,7 +6,6 @@ import Voter from './Voter';
 import { Button } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import * as api from '../utils/api'
-import Loader from './Loader';
 
 
 const H2 = styled.h2`
@@ -27,49 +26,50 @@ const Div = styled.div`
     justify-content:center;
 `
 
+const Article = styled.article`
+    padding: 0.5rem 0;
+    margin: 0.5rem 1rem;
+    border-radius: 10px;
+    color: #F7FFF7;
+    background: ${props => props.color};
+    width: 35vw;
+    min-width: 300px;
+    height: auto;
+    min-height: 200px;
+    border: 2px solid ${props => props.color};
+    font-size: 20px
+    font-family: 'Roboto', sans-serif;
+    display:flex;
+    flex-direction: column;
+    align-items:center;
+`
+const style = {
+    color: `#F7FFF7`,
+    border: `2px solid #F7FFF7`, 
+    width: '100px',
+    margin: '5px',
+    display:'flex',
+    justifyContent: 'center'
+}
+
+
 class ArticleCard extends Component {
 
     state = {
         isDeleted: false
     }
 
-    Article = styled.article`
-        padding: 0.5rem 0;
-        margin: 0.5rem 1rem;
-        border-radius: 10px;
-        color: #F7FFF7;
-        background: ${`${this.props.color}`};
-        width: 35vw;
-        min-width: 300px;
-        height: auto;
-        min-height: 200px;
-        border: 2px solid ${`${this.props.color}`};
-        font-size: 20px
-        font-family: 'Roboto', sans-serif;
-        display:flex;
-        flex-direction: column;
-        align-items:center;
-    `
-
     deleteArticle = ({currentTarget}) => {
         api.deleteArticle(currentTarget.value).then(() => {
             this.setState({isDeleted:true})
         })
     }
-    style = {
-        color: `#F7FFF7`,
-        border: `2px solid #F7FFF7`, 
-        width: '100px',
-        margin: '5px',
-        display:'flex',
-        justifyContent: 'center'
-    }
-
+    
     render() {
         const {articleData:{article_id, title,author,topic,created_at,votes,comment_count},color, user} = this.props
         if(this.state.isDeleted) return <h3>Your article has been deleted</h3>
         return (
-            <this.Article>
+            <Article color={color}>
             <section>
                 <Link id='name' to={`/articles/${article_id}`}>
                     <H2>{(title.length < 40 ? title.toUpperCase() : title.slice(0,40).toUpperCase()+'...')}</H2>
@@ -85,8 +85,8 @@ class ArticleCard extends Component {
             <section>
                 <Voter id={article_id} color={color} votes={votes} type='articles'/>
             </section>
-                {user===author && <Button value={article_id} variant='outlined' startIcon={<DeleteIcon />} style={this.style} onClick={this.deleteArticle}>Delete</Button>}
-        </this.Article>
+                {user===author && <Button value={article_id} variant='outlined' startIcon={<DeleteIcon />} style={style} onClick={this.deleteArticle}>Delete</Button>}
+        </Article>
         );
     }
 }
