5a0e002c03b0af7165f1335ad0cfcd040f8e8bc3
Robin Pillar <rpillar72@gmail.com>
2020-01-06 12:51:59 +0000

    moved styled components outside of commentCard component

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -7,27 +7,35 @@ import { Button } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Voter from './Voter';
 
+const Section = styled.section`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    padding: 0.5rem 0;
+    margin: 0.5rem 1rem;
+    border-radius: 10px;
+    color: #F7FFF7;
+    background: ${props => props.color};
+    width: 40vw;
+    min-width: 300px;
+    height: auto;
+    border: 2px solid ${props => props.color};
+    font-family: 'Roboto', sans-serif;
+`
+
+const style = {
+    color: `#F7FFF7`,
+    border: `2px solid #F7FFF7`, 
+    width: '100px',
+    margin: '5px',
+    display:'flex',
+    justifyContent: 'center'
+}
+
 class CommentCard extends Component {
-    
     state = {
         isDeleted: false
     }
-
-    Section = styled.section`
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-        padding: 0.5rem 0;
-        margin: 0.5rem 1rem;
-        border-radius: 10px;
-        color: #F7FFF7;
-        background: ${this.props.color};
-        width: 40vw;
-        min-width: 300px;
-        height: auto;
-        border: 2px solid ${this.props.color};
-        font-family: 'Roboto', sans-serif;
-    `
 
     handleClick = (event) => {
         return api.deleteComment(event.currentTarget.name).then(() => {
@@ -35,27 +43,18 @@ class CommentCard extends Component {
         })
     }
 
-    style = {
-        color: `#F7FFF7`,
-        border: `2px solid #F7FFF7`, 
-        width: '100px',
-        margin: '5px',
-        display:'flex',
-        justifyContent: 'center'
-    }
-
     render() {
         const {comment:{author,body,created_at,comment_id,votes},user,color} = this.props
   
         if(this.state.isDeleted) return <h3>Your comment has been deleted</h3>
         return (
-            <this.Section>
+            <Section color={color}>
                 <h5><Link to={`/user/${author}`}>{author}</Link></h5>
                 <p>{body}</p>
                 <p>Posted: {moment(created_at).fromNow()}</p>
                 <Voter id={comment_id} color={color} votes={votes} type='comments'/>
-                {user===author && <Button name={comment_id} variant='outlined' startIcon={<DeleteIcon />} style={this.style} onClick={this.handleClick}>Delete</Button>}
-            </this.Section>
+                {user===author && <Button name={comment_id} variant='outlined' startIcon={<DeleteIcon />} style={style} onClick={this.handleClick}>Delete</Button>}
+            </Section>
         );
     }
 }
